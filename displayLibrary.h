c31b2632c26018e568d2ae89c1377d4268bf5c4c
Daniel Canov <dcanov@me.com>
2020-06-11 13:36:28 +0200

    Update displayLibrary.h

--- displayLibrary.h
+++ displayLibrary.h
@@ -1,4 +1,70 @@
 #ifndef displayLibrary
 
+//definitions
+#define SIZE 8
+
+//functions
+void inputError()
+{
+  //InputError - No signal comming from Sensor -
+  //Display NI on 8x8 LedMatrix color red
+
+  char ledMatrixInputError[SIZE][SIZE] =
+  {
+  {'-','-','-','-','-','-','-','-'},
+  {'-','-','-','-','-','-','-','-'},
+  {'-','-','-','-','-','-','-','-'},
+  {'-','R','-','-','R','-','R','-'},
+  {'-','R','R','-','R','-','R','-'},
+  {'-','R','-','R','R','-','R','-'},
+  {'-','R','-','-','R','-','R','-'},
+  {'-','R','-','-','R','-','R','-'}
+};
+  for (size_t i = 0; i < SIZE; i++) {
+    for (size_t j = 0; j < SIZE; j++) {
+      printf("%c", ledMatrixInputError[i][j]);
+      printf("  ");
+    }
+    printf("\n");
+  }
+}
+
+void zahlenAusgabe()
+{
+
+}
+
+void download()
+{
+  char ledMatrixDownloadArrow[SIZE][SIZE] =
+  {
+  {'-','-','-','G','G','-','-','-'},
+  {'-','-','-','G','G','-','-','-'},
+  {'-','-','-','G','G','-','-','-'},
+  {'G','-','-','G','G','-','-','G'},
+  {'-','G','-','G','G','-','G','-'},
+  {'-','-','G','G','G','G','-','-'},
+  {'-','-','-','G','G','-','-','-'}
+};
+    for (size_t i = 0; i < SIZE; i++) {
+      for (size_t j = 0; j < SIZE; j++) {
+        printf("%c", ledMatrixDownloadArrow[i][j]);
+        printf("  ");
+      }
+      printf("\n");
+    }
+  }
+
+
+void lowOxygen()
+{
+
+}
+
+void lowOxygenAlarm()
+{
+
+}
+
 #define displayLibrary
 #endif
