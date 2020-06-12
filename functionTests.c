e97acba20aeef0b73f7e921b0e70f5893de9ce75
Daniel Canov <dcanov@me.com>
2020-06-12 08:18:57 +0200

    functionTests
    Added a menue
    tested it
    working program

--- functionTests.c
+++ functionTests.c
@@ -9,35 +9,39 @@
 //CONSTANTS
 #define SIZE 8
 //VARIABLES
-
+int a;
 //ARRAYS
 
 //PROTOTYPES
-
+void numberSplitFunctionTest();
+void displayLibraryTest();
 
 int main(void) {
-
-  //################################################################
-
-  //Testing section for numberSplitFunction
+  while (1) {
   printf("\n");
   printf("######################################\n");
-  printf("--TESTING SECTION FOR numberSplitFunction--\n");
-  printf("\n");
-  //Tests
-  printf("Enter a number[00-99]: ");
-  scanf("%d", &splitNumberVariable);
-
-  numberSplitFunction(splitNumberVariable);
-  for (size_t i = 0; i < 2; i++) {
-    printf("%d", splitArray[i] );
+  printf("--TESTER--\n");
+  printf("What function or library would you like to test?\n");
+  printf("[1] numberSplitFunction\n");
+  printf("[2] displayLibrary\n");
+  printf("[Other number] = EXIT\n");
+  scanf("%d", &a);
+    switch (a) {
+      case 1:
+        numberSplitFunctionTest();
+        break;
+      case 2:
+        displayLibraryTest();
+        break;
+      default:
+        return 0;
+        break;
+    }
   }
-  printf("\n");
-  printf("######################################\n");
-  printf("\n");
-  //################################################################
+}
 
-  //################################################################
+void displayLibraryTest() {
+
   //Testing section for displayLibrary functions
   printf("\n");
   printf("######################################\n");
@@ -45,10 +49,10 @@ int main(void) {
   printf("\n");
   //Tests
       printf("--PROGRAM STATUS TEST--\n");
-      printf("1 = Normal\n");
-      printf("2 = inputError\n");
-      printf("3 = download\n");
-      printf("Other number = EXIT\n");
+      printf("[1] = Normal\n");
+      printf("[2]= inputError\n");
+      printf("[3] = download\n");
+      printf("[Other number] = EXIT\n");
 
     while (1<2) {
       int status;
@@ -75,12 +79,31 @@ int main(void) {
         case 6:
           zahlenAusgabe(88);
         default:
-          return 0;
+          return;
           break;
       }
     }
     printf("\n");
     printf("######################################\n");
     printf("\n");
-  //##############################################################
+}
+
+void numberSplitFunctionTest() {
+
+    //Testing section for numberSplitFunction
+    printf("\n");
+    printf("######################################\n");
+    printf("--TESTING SECTION FOR numberSplitFunction--\n");
+    printf("\n");
+    //Tests
+    printf("Enter a number[00-99]: ");
+    scanf("%d", &splitNumberVariable);
+
+    numberSplitFunction(splitNumberVariable);
+    for (size_t i = 0; i < 2; i++) {
+      printf("%d", splitArray[i] );
+    }
+    printf("\n");
+    printf("######################################\n");
+    printf("\n");
 }
